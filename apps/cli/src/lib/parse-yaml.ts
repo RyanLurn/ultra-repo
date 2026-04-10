6505f074b8b2fc61b1b53b4891f7c81dca1176d5
Ryan Luong <lumintriet@gmail.com>
2026-04-10 11:28:16 +0700

    feat(cli/utils): define parseYaml function.

--- apps/cli/src/lib/parse-yaml.ts
+++ apps/cli/src/lib/parse-yaml.ts
@@ -1,6 +1,11 @@
-import type { YAMLParseError } from "yaml";
+import type { Result } from "@ultra-repo/core/types/result";
 
+import {
+  createFallbackError,
+  type FallBackError,
+} from "@ultra-repo/core/error/classes/fallback";
 import { BaseError } from "@ultra-repo/core/error/classes/base";
+import { YAMLParseError, parse } from "yaml";
 
 export class InvalidYamlError extends BaseError {
   declare code: "INVALID_YAML_ERROR";
@@ -18,5 +23,40 @@ export class InvalidYamlError extends BaseError {
       context,
       cause,
     });
+  }
+}
+
+export function parseYaml({
+  yamlString,
+}: {
+  yamlString: string;
+}): Result<unknown, InvalidYamlError | FallBackError> {
+  const context = {
+    arguments: {
+      yamlString,
+    },
+    operation: "parseYaml",
+  };
+
+  try {
+    const value = parse(yamlString) as unknown;
+    return {
+      success: true,
+      data: value,
+    };
+  } catch (error) {
+    if (error instanceof YAMLParseError) {
+      const invalidYamlError = new InvalidYamlError({ cause: error, context });
+      return {
+        error: invalidYamlError,
+        success: false,
+      };
+    }
+
+    const fallbackError = createFallbackError({ cause: error, context });
+    return {
+      error: fallbackError,
+      success: false,
+    };
   }
 }
