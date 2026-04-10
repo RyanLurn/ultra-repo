ca019654f03259935c55859e5090e465604aa562
Ryan Luong <lumintriet@gmail.com>
2026-04-10 20:28:54 +0700

    feat(cli/lib): wrap Node's writeFile function in try/catch.

---
+++ apps/cli/src/lib/safe-write-file.ts
@@ -0,0 +1,47 @@
+import type { ObjectEncodingOptions, PathLike, OpenMode, Mode } from "node:fs";
+import type { Result } from "@ultra-repo/core/types/result";
+import type { Abortable } from "node:events";
+import type Stream from "node:stream";
+
+import {
+  createFallbackError,
+  type FallBackError,
+} from "@ultra-repo/core/error/classes/fallback";
+import { type FileHandle, writeFile } from "node:fs/promises";
+
+export async function safeWriteFile({
+  options,
+  file,
+  data,
+}: {
+  options?:
+    | ({
+        flag?: undefined | OpenMode;
+        flush?: undefined | boolean;
+        mode?: undefined | Mode;
+      } & ObjectEncodingOptions &
+        Abortable)
+    | BufferEncoding
+    | null;
+  data:
+    | AsyncIterable<NodeJS.ArrayBufferView | string>
+    | Iterable<NodeJS.ArrayBufferView | string>
+    | NodeJS.ArrayBufferView
+    | string
+    | Stream;
+  file: FileHandle | PathLike;
+}): Promise<Result<undefined, FallBackError>> {
+  try {
+    await writeFile(file, data, options);
+    return {
+      data: undefined,
+      success: true,
+    };
+  } catch (error) {
+    const fallbackError = createFallbackError({ cause: error });
+    return {
+      error: fallbackError,
+      success: false,
+    };
+  }
+}
